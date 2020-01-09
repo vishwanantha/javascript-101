fbec7e11b58a0fe54180e17558641fc1eb8f34e5
reach2arunprakash <reach2arunprakash@gmail.com>
2020-01-09 16:29:25 +0530

    Update scripts.js

--- workouts/jsontask/scripts.js
+++ workouts/jsontask/scripts.js
@@ -3,7 +3,7 @@ var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
-
+request.send();
 request.onload = function() {
   // Begin accessing JSON data here
 var data = JSON.parse(this.response)
