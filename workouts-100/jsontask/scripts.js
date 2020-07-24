946961a1efa4586d03fec56622b5c7a6342cced6
arun <reach2arunprakash@gmail.com>
2020-07-24 21:45:48 +0530

    fetch

--- workouts-100/jsontask/scripts.js
+++ workouts-100/jsontask/scripts.js
@@ -1,11 +1,19 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
-
+var url_string = 'https://restcountries.eu/rest/v2/all';
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
+request.open('GET',url_string , true)
 request.send();
 request.onload = function() {
   // Begin accessing JSON data here
 var data = JSON.parse(this.response)
 console.log(data);
 }
+
+/*
+fetch(url_string) // Call the fetch function passing the url of the API as a parameter
+.then(res=>res.json())
+.then(data=>console.log(data))
+*/
+
+
